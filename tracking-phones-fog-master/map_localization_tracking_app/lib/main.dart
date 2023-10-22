a3f661d1ecb0fb5b7ace64bb10b541c66aa71288
Mohamd Ben Rhouma <mohamed.benrhouma@enis.tn>
2023-10-22 18:43:04 +0100

    adding my Project to GitHub

---
+++ tracking-phones-fog-master/map_localization_tracking_app/lib/main.dart
@@ -0,0 +1,212 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController phoneNumberController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: phoneNumberController,
+              decoration: InputDecoration(
+                labelText: 'Enter your phone number',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final phoneNumber = phoneNumberController.text;
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MyMapPage(phoneNumber: phoneNumber),
+                  ),
+                );
+              },
+              child: Text('Login and Start Map'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyMapPage extends StatefulWidget {
+  final String phoneNumber;
+
+  MyMapPage({required this.phoneNumber});
+
+  @override
+  _MyMapPageState createState() => _MyMapPageState();
+}
+
+class _MyMapPageState extends State<MyMapPage> {
+  late GoogleMapController mapController;
+  final LatLng _center = const LatLng(34.7282712, 10.7155989);
+  List<Marker> markers = [];
+  String srvUrl = '';
+  late Timer markerRemovalTimer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    markerRemovalTimer = Timer.periodic(const Duration(seconds: 60), (timer) {
+      removeMarkers();
+    });
+
+    _trackMe();
+  }
+
+  void removeMarkers() {
+    setState(() {
+      markers.clear();
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  double randomHue() {
+    final random = Random();
+    return random.nextDouble() * 360.0;
+  }
+
+  Future<String> _getIPAddress() async {
+    final response = await http.get(Uri.parse('https://api.ipify.org?format=json'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['ip'];
+    } else {
+      return 'Unknown';
+    }
+  }
+
+  Future<void> _publishLocation(double latitude, double longitude, String phoneNumber) async {
+    try {
+      final payload = {
+        'id': phoneNumber, // Use the phone number as the ID
+        'content': {'latitude': latitude, 'longitude': longitude}
+      };
+      String message = json.encode(payload);
+
+      final response = await http.post(
+        Uri.parse(srvUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: message,
+      );
+
+      if (response.statusCode == 200) {
+        final responseData = json.decode(response.body);
+
+        for (var item in responseData) {
+          var id = item['id'];
+          var lat = item['content']['latitude'];
+          var long = item['content']['longitude'];
+
+          setState(() {
+            markers.add(Marker(
+              markerId: MarkerId(id),
+              position: LatLng(lat, long),
+              infoWindow: InfoWindow(
+                title: '$lat$long',
+                snippet: 'Lat: $lat, Lng: $long',
+              ),
+              icon: BitmapDescriptor.defaultMarkerWithHue(randomHue()),
+            ));
+          });
+        }
+      } else {
+        print('Failed to send request. Status code: ${response.statusCode}');
+        srvUrl=await getUrl();
+      }
+    } catch (e) {
+      print('Error sending request: $e');
+    }
+  }
+  Future<String> getUrl() async {
+    List<String> servers = [
+      'http://10.25.14.172:5000/position',
+      'http://10.26.13.94:5000/position',
+      'http://10.25.13.25:5000/position'
+    ];
+
+    // Generate a random index to select a server
+    final random = Random();
+    final randomIndex = random.nextInt(servers.length);
+
+    // Return the randomly selected server URL
+    return servers[randomIndex];
+  }
+
+  Future<void> _trackMe() async {
+    final String ipAddress = await _getIPAddress();
+    //srvUrl = 'http://10.26.13.94:5000/position';
+  srvUrl = await getUrl();
+    Timer.periodic(const Duration(seconds: 5), (timer) async {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      print("Latitude: ${position.latitude}");
+      print("Longitude: ${position.longitude}");
+      await _publishLocation(position.latitude, position.longitude, widget.phoneNumber);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    markerRemovalTimer.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Maps Localization Tracking For Phones'),
+        backgroundColor: Colors.green[700],
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 13.0,
+        ),
+        markers: Set<Marker>.from(markers),
+      ),
+    );
+  }
+}
---
+++ venv Server/src/main.py
@@ -0,0 +1,195 @@
+from flask import Flask, json, request, jsonify, send_file
+from flask_cors import CORS
+import time
+from multiprocessing import Process
+
+# tebe3 serverur socket
+import socket
+import json
+
+
+import requests
+
+
+app = Flask(__name__)
+CORS(app)
+
+hostSocket1 = "0.0.0.0"  # Listen on all available network interfaces
+portSocket1 = 65431
+
+hostSocket2 = "0.0.0.0"  # Listen on all available network interfaces
+portSocket2 = 65432
+
+
+# Define the host and port of the server
+senderId1 = 1
+senderIp1 = "10.26.13.94"  # Replace with the server's IP address amine
+senderPort1 = 65431
+
+# Define the host and port of the server
+senderId2 = 2
+senderIp2 = "10.25.14.172"  # Replace with the server's IP address dali
+senderPort2 = 65432
+position_data = []
+
+id_list = []
+
+
+# Define a function to remove the earliest element from the list every 15 seconds
+def remove_oldest_data():
+    global position_data, id_list
+    while True:
+        position_data = []
+        id_list = []
+        # Write the updated list to the file
+        with open("position_data.json", "w") as json_file1:
+            json.dump(position_data, json_file1)
+        with open("received_position_data1.json", "w") as json_file2:
+            json.dump(position_data, json_file2)
+        with open("received_position_data2.json", "w") as json_file3:
+            json.dump(position_data, json_file3)
+
+        print("Old data removed!")
+        time.sleep(45)
+
+
+def socket_send(host, port):
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind((host, port))
+            server_socket.listen()
+            print("Server listening on", (host, port))
+            conn, addr = server_socket.accept()
+            print("Connected by", addr)
+
+            with open("position_data.json", "r") as position_json:
+                position = json.load(position_json)
+
+            # Serialize the JSON data to send
+            json_message = json.dumps(position)
+
+            try:
+                # Send the JSON message to the client
+                conn.sendall(json_message.encode())
+                print("Sending...........")
+            except Exception as e:
+                print("An error occurred:", str(e))
+
+            finally:
+                conn.close()
+
+            time.sleep(10)
+
+
+def socket_receive(id, host, port):
+    while True:
+        try:
+            # Create a socket and connect to the server
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.settimeout(5)  # Set a timeout for the connection
+
+            while True:
+                try:
+                    client_socket.connect((host, port))
+                    break
+                except (ConnectionRefusedError, TimeoutError):
+                    print("Server is not available. Retrying in 5 seconds...")
+                    time.sleep(5)
+
+            # Receive the server's JSON message
+            json_message = ""
+            while True:
+                chunk = client_socket.recv(1024).decode()
+                if not chunk:
+                    break  # No more data received
+                json_message += chunk
+
+            # Deserialize the JSON message
+            try:
+                json_data = json.loads(json_message)
+                print("=====================")
+                print("Received JSON message from the server:", json_data)
+                print("=====================")
+
+                # Write the updated list to the file
+                file_name = f"received_position_data{str(id)}.json"
+                with open(file_name, "w") as json_file:
+                    json.dump(json_data, json_file)
+            except json.JSONDecodeError as e:
+                print("Error decoding JSON message:", str(e))
+        except KeyboardInterrupt:
+            print("Client interrupted. Exiting...")
+            break
+        except Exception as e:
+            print("An error occurred:", str(e))
+        finally:
+            client_socket.close()
+        time.sleep(10)
+
+
+# flutter request
+@app.route("/position", methods=["POST"])
+def receive_position():
+    global position_data
+
+    # Get the JSON data from the request
+    data = request.get_json()
+
+    # Add a timestamp to the data
+    data["timestamp"] = time.time()
+    id_list.append(id)
+
+    # Append the updated data to the list
+    position_data.append(data)
+    print("===========================")
+    print("position data list: ", data)
+    print("===========================")
+
+    # Read the existing data from the file
+    with open("position_data.json", "r") as json_file:
+        existing_data = json.load(json_file)
+
+    # Append the new data to the existing data
+    existing_data.append(data)
+
+    # Write the updated data to the file
+    with open("position_data.json", "w") as json_file1:
+        json.dump(existing_data, json_file1)
+
+    with open("received_position_data1.json", "r") as json_file2:
+        existing_data2 = json.load(json_file2)
+
+    with open("received_position_data2.json", "r") as json_file3:
+        existing_data3 = json.load(json_file3)
+
+    return jsonify(existing_data + existing_data2 + existing_data3), 200
+
+
+if __name__ == "__main__":
+    # Create processes for each function
+    socket_receive_process1 = Process(
+        target=socket_receive, args=(senderId1, senderIp1, senderPort1)
+    )
+    socket_receive_process2 = Process(
+        target=socket_receive, args=(senderId2, senderIp2, senderPort2)
+    )
+    socket_send_process1 = Process(target=socket_send, args=(hostSocket1, portSocket1))
+    socket_send_process2 = Process(target=socket_send, args=(hostSocket2, portSocket2))
+    remove_process = Process(target=remove_oldest_data)
+
+    # Start the processes
+    socket_receive_process1.start()
+    socket_receive_process2.start()
+    socket_send_process1.start()
+    socket_send_process2.start()
+    remove_process.start()
+
+    # Start the Flask app in the main thread
+    app.run(host=hostSocket1, port=5000)
+
+    # Wait for the processes to finish
+    socket_receive_process1.join()
+    socket_receive_process2.join()
+    socket_send_process1.join()
+    socket_send_process2.join()
+    remove_process.join()
